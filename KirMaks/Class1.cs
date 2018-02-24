a6569fa24ee2fceaa35e89f1f31b9cc220cccee4
Student <Student@EMIS.local>
2018-02-24 19:35:53 +0700

    sss

---
+++ KirMaks/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KirMaks
+{
+    abstract class Class1
+    {
+        public abstract void Draw(PictureBox cc);
+    }
+}
---
+++ KirMaks/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KirMaks
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Class2 g = new Class2();
+            g.Draw(pictureBox1);
+        }
+
+        private void pictureBox1_DoubleClick(object sender, EventArgs e)
+        {
+            Class3 g = new Class3();
+            g.Draw(pictureBox1);
+        }
+    }
+}
